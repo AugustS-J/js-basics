35ee1cae2dc90d7006cc2c34f99990625462beb2
AugustS-J <ausc0001@stud.kea.dk>
2023-08-23 15:26:47 +0200

    Øvelse 2

--- main.js
+++ main.js
@@ -26,3 +26,18 @@ colaKnap.addEventListener("click", function () {
 faxeKnap.addEventListener("click", function () {
   checkDrinkType("faxe");
 });
+
+function faaHilsen(time) {
+  if (time >= 5 && time < 10) {
+    console.log("Godmorgen");
+  } else if (time >= 10 && time < 18) {
+    console.log("Goddag");
+  } else if (time >= 18 && time < 24) {
+    console.log("Godaften");
+  } else {
+    console.log("Godnat");
+  }
+}
+
+const nuvaerendeTime = new Date().getHours();
+const hilsen = faaHilsen(nuvaerendeTime);
